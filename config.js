3f199a485fa625f96506edfa918d1f7ef0606958
U-TAOBAO-HZ\sunfang.pt <sunfang.pt@sunfang.taobao.ali.com>
2011-11-14 10:53:15 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+exports.buffer_size = 500*1024*1024;   //最大的队列数据长度,单位B
+exports.max_item_number = 500*1024;   //最大消息数量
+exports.item_max_size = 1000;         //单个消息最大长度，单位B
+exports.server_port = 8888;          //服务器运行端口
---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * MQ.js: Memory Message Quene Service based on Node.js
+ * sunfang.pt@taobao.com
+ */
+
+var server = require('./lib/server');
+server.start();
+
