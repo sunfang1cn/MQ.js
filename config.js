e85eeabda800666964c8f393a3853ac418c9d8a1
Sun Fang <sunfang1cn@yahoo.com.cn>
2011-11-16 11:43:00 +0800

    add sync disk config

--- config.js
+++ config.js
@@ -1,5 +1,16 @@
-exports.buffer_size = 100*1024*1024;   //最大的队列数据长度,单位B
-exports.max_item_number = 100*1024;   //最大消息数量
-exports.item_max_size = 1000;         //单个消息最大长度，单位B
-exports.server_port = 8888;          //服务器运行端口
-exports.flush_dir = '.';            //存储映像的目录
+exports.buffer_size = 100*1024*1024;    //Maxmium msg queue length(B)
+exports.max_item_number = 100*1024;    //Maxmium msg count
+exports.item_max_size = 1000;         //Maxmium size per msg(B)
+
+exports.server_port = 8888;          //the service network port
+
+exports.flush_dir = '.';            //the dictionary which save the flush files, must be exists
+
+/*
+ * Policy which control how to synchronize between memory queue data and disk files : 
+ *  'none' : donnot do any synchronize automaticly. you can use 'flushall' and 'readall' command manually;
+ *  'always' : any change will be synchronized into disk files instantly, this may produce lots of io cost.
+ *  'time' : by this it will do synchronizing per time which defined in the option flush_time(mili-second).
+ */
+exports.flush_policy = 'none';
+exports.flush_time = 2500;
