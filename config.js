e2f5d19de772184a6aa647786817fd40fc6b06a3
Sun Fang <sunfang1cn@yahoo.com.cn>
2011-11-16 10:27:02 +0800

    add completed flush function

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.buffer_size = 1024*1024;   //最大的队列数据长度,单位B
-exports.max_item_number = 1024;   //最大消息数量
+exports.buffer_size = 100*1024*1024;   //最大的队列数据长度,单位B
+exports.max_item_number = 100*1024;   //最大消息数量
 exports.item_max_size = 1000;         //单个消息最大长度，单位B
 exports.server_port = 8888;          //服务器运行端口
 exports.flush_dir = '.';            //存储映像的目录
